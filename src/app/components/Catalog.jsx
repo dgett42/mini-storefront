4dc23febda5dc482851957b38f0241c457bd491a
Donovan <degeter1@usf.edu>
2025-11-02 18:55:05 -0500

    I imported the React hooks useEffect, useMemo, and useState to manage state and rendering. I also imported the main components for the storefront: ProductList, CategoryFilter, PriceFilter, CartSummary, and StatusMessage. These will be used to build out the main catalog interface. In addition, I cleaned up the file structure by adding new folders.

---
+++ src/app/components/Catalog.jsx
@@ -0,0 +1,7 @@
+'use client';
+import { useEffect, useMemo, useState } from 'react';
+import ProductList from './ProductList';
+import CategoryFilter from './CategoryFilter';
+import PriceFilter from './PriceFilter';
+import CartSummary from './CartSummary';
+import StatusMessage from './StatusMessage';
